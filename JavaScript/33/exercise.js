a940dda21da7342575d40cfd0d48903d09a5a85c
SSbrilly <Simonesbrilli@gmail.com>
2023-06-14 02:58:14 +0200

    commit for pc change

---
+++ JavaScript/33/exercise.js
@@ -0,0 +1,19 @@
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+// }
+
+// const developer = new Person("Mario", "Rossi");
+// console.log(developer.firstName + " " + developer.lastName);
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+const developer = new Person("Mario", "Rossi");
+console.log(`${developer.firstName} ${developer.lastName}`);
---
+++ JavaScript/36/exercise.js
@@ -0,0 +1,9 @@
+const interval = setInterval(repeatHello, 5000);
+
+function repeatHello() {
+  console.log("Hello");
+}
+
+
+clearInterval(interval);
+
