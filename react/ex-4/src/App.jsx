75f01c81ef28d6453e59e41286c6bd817ced2b9b
Simone Sbrilly <simonesbrilli@gmail.com>
2023-08-20 14:17:50 +0200

    react ex 44 done

--- react/ex-4/src/App.jsx
+++ react/ex-4/src/App.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./App.css";
-import FilteredList from "./FilteredList";
+import Counter from "./Counter";
 
 function App() {
-  const items = [
-    { id: 1, name: "Alice", age: 25 },
-    { id: 2, name: "Bob", age: 30 },
-    { id: 3, name: "Charlie", age: 18 },
-  ];
-
   return (
-    <div className="App">
-      <h1>FilteredList App</h1>
-      <FilteredList items={items} />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>React App</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/counter">Counter</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/counter" component={Counter} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
