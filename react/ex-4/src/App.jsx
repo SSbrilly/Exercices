4bc9e36a86bd84242f9b7403b281e19e29943e6e
Simone Sbrilly <simonesbrilli@gmail.com>
2023-08-20 13:35:46 +0200

    react ex 37 done

--- react/ex-4/src/App.jsx
+++ react/ex-4/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import CounterComponent from "./CounterComponent";
+import FormComponent from "./Form";
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
-      <CounterComponent />
+      <FormComponent />
     </div>
   );
 }
