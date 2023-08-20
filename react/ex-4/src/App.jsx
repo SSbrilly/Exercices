2eeb42232742e6e66c76ef2b2721ad57bf6e943e
Simone Sbrilly <simonesbrilli@gmail.com>
2023-08-20 13:58:42 +0200

    react ex 42 done

--- react/ex-4/src/App.jsx
+++ react/ex-4/src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
-import LanguageContext from "./LanguageContext";
-import DisplayLanguage from "./DisplayLanguage";
+import CounterComponent from "./CounterComponent";
 
 function App() {
-  const selectedLanguage = "English";
   return (
     <div className="App">
-      <h1>Language App</h1>
-      <LanguageContext.Provider value={selectedLanguage}>
-        <DisplayLanguage />
-      </LanguageContext.Provider>
+      <h1>React App</h1>
+      <CounterComponent />
     </div>
   );
 }
---
+++ react/ex-4/src/CounterComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import useCounter from "./useCounter";
+
+function CounterComponent() {
+  const { count, increment, decrement, reset } = useCounter(0);
+
+  return (
+    <div>
+      <h2>Counter Component</h2>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default CounterComponent;
