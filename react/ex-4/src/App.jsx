3b91caa64311a1d72b4ab26bc76e5a5122e44c2a
Simone Sbrilly <simonesbrilli@gmail.com>
2023-08-20 12:02:25 +0200

    react ex-11 done

--- react/ex-4/src/App.jsx
+++ react/ex-4/src/App.jsx
@@ -5,7 +5,8 @@ import Welcome from "./Welcome";
 function App() {
   return (
     <div className="App">
-      <Welcome name={<strong>Simone</strong>} age={30} />
+      <Welcome name="John" age={30} />
+      <Welcome name="Alice" age={15} />
     </div>
   );
 }
