526c8b9fe9862828c535672bf8c686470dcad712
Simone Sbrilly <simonesbrilli@gmail.com>
2023-08-20 13:06:21 +0200

    react ex 30 done

--- react/ex-4/src/App.jsx
+++ react/ex-4/src/App.jsx
@@ -1,30 +1,12 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
-import DisplayLanguage from "./DisplayLanguage";
-import { LanguageProvider, LanguageContext } from "./LanguageContext";
+import ClickCounter from "./ClickCounter";
 
 function App() {
-  const languages = ["English", "Spanish", "French"];
-  const { setSelectedLanguage } = useContext(LanguageContext);
-
-  const handleLanguageChange = (event) => {
-    setSelectedLanguage(event.target.value);
-  };
-
   return (
-    <LanguageProvider>
-      <div className="App">
-        <h1>Language Selector</h1>
-        <select onChange={handleLanguageChange}>
-          {languages.map((language, index) => (
-            <option key={index} value={language}>
-              {language}
-            </option>
-          ))}
-        </select>
-        <DisplayLanguage />
-      </div>
-    </LanguageProvider>
+    <div className="App">
+      <ClickCounter />
+    </div>
   );
 }
 
