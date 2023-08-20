6bdea7812990bcf95e825cc005cb9e88699a7894
Simone Sbrilly <simonesbrilli@gmail.com>
2023-08-20 11:25:50 +0200

    react ex 2

---
+++ react/exercise-2/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <>
+      <div classname="App">
+        <Welcome name="Simone" />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ react/exercise-2/src/Welcome.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    const { name } = this.props;
+    return <p>Welcome, {name}!</p>;
+  }
+}
+
+export default Welcome
