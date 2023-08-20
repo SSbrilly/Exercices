62e6e65cb8e88176bd6c98bd8cd738c6c79dd01f
Simone Sbrilly <simonesbrilli@gmail.com>
2023-08-20 14:35:34 +0200

    react ex 51 done

--- react/ex-4/src/fetchGithubUser.js
+++ react/ex-4/src/fetchGithubUser.js
@@ -1,12 +1,20 @@
 import useSWR from "swr";
 
-function fetchGithubUser(username) {
-  return fetch(`https://api.github.com/users/${username}`).then((response) =>
-    response.json()
-  );
+async function fetchGithubUser(username) {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const data = await response.json();
+  return data;
 }
 
 function useGithubUser(username) {
+  if (!username) {
+    return {
+      user: null,
+      isLoading: false,
+      isError: false,
+    };
+  }
+
   const { data, error } = useSWR(username, fetchGithubUser);
 
   return {
