4a164380efa38cca5c901a3f226f56f3ebe4902a
Simone Sbrilly <simonesbrilli@gmail.com>
2023-09-07 00:23:59 +0200

    exercises added and done from 1 to 6

---
+++ node/2/2a.js
@@ -0,0 +1,6 @@
+const addFunction = require('./2')
+
+const result = addFunction(5, 3)
+
+console.log(`The result is: ${result}`);
+
---
+++ node/3/3a.js
@@ -0,0 +1,5 @@
+import func from "./3";
+
+const result = func(7, 3)
+
+console.log(`The result is: ${result}`);
